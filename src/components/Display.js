cb973ba8ed71c7775a57236b27ee0c70685401f0
Moungjung Kim <moungjung@gmail.com>
2020-02-20 20:12:47 -0700

    fixed some styling and added display fields and start button

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import {StyledDisplay} from './styles/StyledDisplay';
 
 const Display = ({gameover, text}) => (
-    <div>{text}</div>
+    <StyledDisplay gameOver={gameover}>{text}</StyledDisplay>
 );
 
 export default Display;
